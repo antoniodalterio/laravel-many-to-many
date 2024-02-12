26f9978d38e449f6ab6ca53d051ca4fa59fed627
antoniodalterio <antoniodalterio2227@gmail.com>
2024-02-12 16:27:28 +0100

    feat: seeder

--- database/seeders/TecnologySeeder.php
+++ database/seeders/TecnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tecnology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class TecnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tecnologies = ['HTML', 'CSS', 'JS', 'VUE', 'PHP', 'LARAVEL'];
+        foreach ($tecnologies as $tecnology) {
+
+            $new_tecnology = new Tecnology();
+            $new_tecnology->title = $tecnology;
+            $new_tecnology->description = $tecnology;
+
+            $new_tecnology->save();
+
+        }
     }
 }
