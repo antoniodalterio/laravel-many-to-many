5fbe43a2e6038183f10d770b24f00946a287c176
antoniodalterio <antoniodalterio2227@gmail.com>
2024-02-12 16:12:37 +0100

    feat: task 1

---
+++ database/seeders/TecnologySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TecnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
